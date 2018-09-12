642036a55e3182a31d0ab3548f3f4441bb2c6909
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-09-12 16:22:17 +0300

    fix

---
+++ src/js/import/parallax.js
@@ -0,0 +1,19 @@
+import Parallax from 'parallax-js';
+
+var scene1 = $('.parallax-1').get(0);
+var intro1 = new Parallax(scene1);
+
+var scene2 = $('.parallax-2').get(0);
+var intro2 = new Parallax(scene2);
+
+
+var scene3 = $('.parallax-3').get(0);
+var intro3 = new Parallax(scene3);
+
+
+var scene4 = $('.parallax-4').get(0);
+var intro4 = new Parallax(scene4);
+
+
+var scene5 = $('.parallax-5').get(0);
+var intro5 = new Parallax(scene5);
